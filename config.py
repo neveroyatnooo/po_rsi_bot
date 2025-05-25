b37f3626c130f8d18fc9dd086afdb042b8b2e1b5
neveroyatnooo <standamabuben@mail.ru>
2025-05-26 03:40:00 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,22 @@
+# config.py
+
+# Учетные данные Pocket Option
+PO_EMAIL        = "tradersumom@mail.ru"
+PO_PASSWORD     = "Nasvai322."
+
+# Telegram Bot
+TELEGRAM_TOKEN  = "7777081301:AAHLuvi5X5LtFmRZsi4mGxTbMLsv6rQ9ex4"
+TELEGRAM_CHAT_ID= 981037533
+
+# Настройки стратегии
+OTC_PAIRS       = [
+    "EURUSD-OTC",
+    "USDJPY-OTC",
+    # добавьте другие пары по необходимости
+]
+RSI_PERIODS     = [7, 14, 21]
+RSI_UPPER       = 70
+RSI_LOWER       = 30
+EXPIRATION_MIN  = 2
+
+
